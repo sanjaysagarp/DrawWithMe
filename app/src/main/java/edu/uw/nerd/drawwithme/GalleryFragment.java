04505ac636fcf09315da2529f289ad1cef319955
ying <whydang@uw.edu>
2016-05-24 01:09:13 -0700

    link add drawing to canvas

--- app/src/main/java/edu/uw/nerd/drawwithme/GalleryFragment.java
+++ app/src/main/java/edu/uw/nerd/drawwithme/GalleryFragment.java
@@ -1,10 +1,12 @@
 package edu.uw.nerd.drawwithme;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 /**
@@ -34,6 +36,8 @@ public class GalleryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.frag_gallery, container, false);
+
+
         return view;
     }
 }
--- app/src/main/java/edu/uw/nerd/drawwithme/HomeActivity.java
+++ app/src/main/java/edu/uw/nerd/drawwithme/HomeActivity.java
@@ -1,9 +1,12 @@
 package edu.uw.nerd.drawwithme;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by DANG on 5/23/2016.
@@ -21,6 +24,22 @@ public class HomeActivity extends AppCompatActivity {
         // Give the TabLayout the ViewPager
         TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
         tabLayout.setupWithViewPager(viewPager);
-
+//
+//        Button new_btn = (Button) getActivity().findViewById(R.id.new_draw_btn);
+//        new_btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getActivity(), MainActivity.class);
+//                intent.putExtra("edu.uw.nerd.drawwithme", "New Drawing Initiated");
+//                startActivity(intent);
+//            }
+//        });
     }
+
+    public void initiateDraw(View v) {
+        Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+        intent.putExtra("edu.uw.nerd.drawwithme", "New Drawing Initiated");
+        startActivity(intent);
+    }
+
 }
