d56a3d318f199c3bba1a80fb522e8414e62265e7
ying <whydang@uw.edu>
2016-06-01 00:59:08 -0700

    working on getting imgur to work

--- app/src/main/java/edu/uw/nerd/drawwithme/InboxFragment.java
+++ app/src/main/java/edu/uw/nerd/drawwithme/InboxFragment.java
@@ -2,7 +2,10 @@ package edu.uw.nerd.drawwithme;
 
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.app.Fragment;
@@ -14,7 +17,16 @@ import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by DANG on 5/23/2016.
@@ -39,14 +51,19 @@ public class InboxFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.frag_inbox, container, false);
-        dir = (File) getArguments().getSerializable(INBOX_PAGE);
 
+        ImageTask imgTask = new ImageTask();
+        imgTask.execute("https://api.imgur.com/3/image/I5yPFUD");
+
+
+        dir = (File) getArguments().getSerializable(INBOX_PAGE);
         GridView grid = (GridView) rootView.findViewById(R.id.gridview);
 
         // TODO: ideally should be Uri but issue with DrawingSurfaceView results in temp files...
@@ -79,4 +96,83 @@ public class InboxFragment extends Fragment {
 
         return rootView;
     }
+
+
+
+    private class ImageTask extends AsyncTask<String, Void, List<String>> {
+
+        public static final String CLIENT_ID = " 76c244ffc9d4c9c";
+        public static final String CLIENT_SECRET = "8dde9266ec353e305169e658957c68099ec229d2";
+
+
+        @Override
+        protected List<String> doInBackground(String... params) {
+
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            List<String> images = new ArrayList<String>();
+
+            Log.v("AM I ENTERING THIS?", "LELELEL");
+            // need to grab a list of all ID associated with this user............
+
+
+            try {
+                URL url = new URL(params[0]);
+
+                // attempt to connect to the url
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setDoOutput(true);
+                urlConnection.setDoInput(true);
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setRequestProperty("Authorization", "Client-ID " + CLIENT_ID);
+                urlConnection.setRequestMethod("GET");
+
+                urlConnection.connect();
+
+                int status = urlConnection.getResponseCode();
+                Log.v("STATUS", "" +status);
+
+                InputStream inputStream = urlConnection.getInputStream();
+                if (inputStream == null) {
+                    return null;
+                }
+
+
+                StringBuffer buffer = new StringBuffer();
+                reader = new BufferedReader(new InputStreamReader((inputStream)));
+
+                // grab each line from the input stream and append onto the buffer
+                String line = reader.readLine();
+                while (line != null) {
+                    buffer.append(line + "\n");
+                    line = reader.readLine();
+                }
+
+                // implies there's nothing to read to begin with
+                if (buffer.length() == 0) {
+                    return null;
+                }
+
+                String results = buffer.toString();
+                Log.v("TESTING", results);
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return images;
+        }
+
+
+
+        @Override
+        protected void onPostExecute(List<String> result) {
+            super.onPostExecute(result);
+            // take each string ID(?) of images and display em on the grid here
+        }
+    }
+
+
 }
